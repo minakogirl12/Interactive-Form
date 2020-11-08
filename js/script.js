7302d96ed42a5bb49399fea84d533184cefb0fb2
drobinson <30643831+minakogirl12@users.noreply.github.com>
2020-11-08 15:08:33 -0500

    Re wrote the fields
    reduced unused items for readability

--- js/script.js
+++ js/script.js
@@ -1,53 +1,69 @@
-//Select all the DOM input elements into variables
-const userName = document.getElementById('name'); //line of text
-const userEmail = document.getElementById('mail'); //line of text
-const userTitle = document.getElementById('title');  //drop-down menu
-const userOtherTitle = document.getElementById('other-title');
-
 //elements for the shirt
-const userShirtSize = document.getElementById('size');
-const userShirtDesign = document.getElementById('design');
-const userShirtColorDiv = document.querySelector('#shirt-colors');
-const userShirtColor = document.getElementById('color');
-
-//array of all the activities
-const userActivities = document.querySelectorAll('input[type=checkbox]'); //adapted from Stackoverflow code: document.querySelectorAll('input[type=text]')
-
+const userShirtSize = document.getElementById('size'); //drop-down for size
+const userShirtDesign = document.getElementById('design'); //drop-down for designs
+const userShirtColor = document.getElementById('color'); //drop-down for colors
 
 //hide the input field for other job titles
+const userOtherTitle = document.getElementById('other-title');
 userOtherTitle.style.display = "none";
 
-//hide color fields from being visible
-userShirtColorDiv.style.display = "none";
+//hide color fields from being visible and display “Please select a T-shirt theme”.
+userShirtColor.style.display = "none";
+let error = document.createElement('p');
+error.textContent = "Please select a T-shirt theme";
+error.style.color = 'red';
+error.style.fontWeight = 'bold';
+userShirtColor.parentNode.appendChild(error);
 
 //focus on first element by default when page first loads
+const userName = document.getElementById('name');
 userName.focus();
-
-
 
 //add the listener to the shirt drop down
 userShirtDesign.addEventListener('change', () => {
-
     //capture valuse of the drop down into a variable
     let shirtValue = userShirtDesign.value;
     let colors = document.getElementById('color').children; //list of all color elements
-    
+    let selectedItemText = userShirtColor.options[userShirtColor.selectedIndex].text;
     switch(shirtValue){
         case 'Select Theme':
             console.log('select theme selected');
-            userShirtColorDiv.style.display = "none";
+            userShirtColor.style.display = "none";
+            error.style.display = "";
 
             break;
+
         case 'js puns':
             console.log('js puns selected');
-            userShirtColorDiv.style.display = "";
+            userShirtColor.style.display = "";
+            error.style.display = "none";
+            //if selected item is unavailable changed selected item to available color
+            if(selectedItemText.includes('JS Puns')){}
+            //Make sure only js puns colors are available
+            for(let i = 0; i < colors.length; i++){
+                if(colors[i].textContent.includes('JS Puns')){
+                    colors[i].disabled = false;
+                }
+                else{
+                    colors[i].disabled = true;
+                }
+            }
+            //if selected item is unavailable changed selected item to available color
             break;
+
         case 'heart js':
             console.log('heart js selected');
-            userShirtColorDiv.style.display = "";
+            userShirtColor.style.display = "";
+            error.style.display = "none";
+
+            for(let i = 0; i < colors.length; i++){
+                if(colors[i].textContent.includes('JS Puns')){
+                    colors[i].disabled = true;
+                }
+                else{
+                    colors[i].disabled = false;
+                }
+            }
             break;
-
-    }
-
-
-});
+        }}
+        );
